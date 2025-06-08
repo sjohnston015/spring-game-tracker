8deea1216bb265fde87cd1f1a2f181381b25e100
Seth Johnston <sjohnston@seths-macbook.local>
2025-06-08 18:15:56 -0400

    commit all initial project files

---
+++ src/main/java/com/sethjohnston/gametracker/GametrackerApplication.java
@@ -0,0 +1,12 @@
+package com.sethjohnston.gametracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GametrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GametrackerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/sethjohnston/gametracker/GametrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sethjohnston.gametracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GametrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
